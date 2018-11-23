dc70bbb7b9d3391d56d24f838e29d0f52d55b618
Dan Barrett <dan@iter8.com.au>
2018-11-23 14:21:16 +1100

    Added better support for Symfony 3.4 vs 4.1

--- src/helpers.php
+++ src/helpers.php
@@ -1,5 +1,30 @@
 <?php
 
+use Symfony\Component\VarDumper\VarDumper;
+
+// Check to see if we have the >=4.1 VarDumper version which has ServerDumper (and includes its own version of dd)
+if (!function_exists('dd') && !class_exists('\Symfony\Component\VarDumper\Dumper\ServerDumper')) {
+    /**
+     * Nicely formatted var_dump and die.
+     * Hat tip Laravel.
+     *
+     * @param mixed $var
+     * @param mixed $moreVars
+     *
+     * @return void
+     */
+    function dd($var, ...$moreVars)
+    {
+        VarDumper::dump($var);
+
+        foreach ($moreVars as $v) {
+            VarDumper::dump($v);
+        }
+
+        exit(1);
+    }
+}
+
 if (!function_exists('ddl')) {
     /**
      * Echo the line number before dying.
