1f3233a8420fdd9bfa796259198c9b005d9e78ee
Dan Barrett <dan@iter8.com.au>
2018-11-23 12:59:16 +1100

    Don't overwrite the Symfony dd

--- src/helpers.php
+++ src/helpers.php
@@ -1,24 +1,5 @@
 <?php
 
-if (!function_exists('dd')) {
-    /**
-     * Nicely formatted var_dump and die.
-     * Hat tip Laravel.
-     *
-     * @param mixed ...$args
-     *
-     * @return void
-     */
-    function dd(...$args): void
-    {
-        foreach ($args as $arg) {
-            (new Symfony\Component\VarDumper\Dumper\HtmlDumper())->dump($arg);
-        }
-
-        die(1);
-    }
-}
-
 if (!function_exists('ddl')) {
     /**
      * Echo the line number before dying.
